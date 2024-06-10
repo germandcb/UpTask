2e3eb011d26b49d5620305bd646e9b761350c7b2
germandc <germancelis16@gmail.com>
2024-06-10 09:00:41 -0500

    Creando vista de login y crear cuenta

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,25 +2,35 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController {
-    public static function login() {
-        echo "Desde Login";
+    public static function login(Router $router) {
 
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         }
+
+        // Renderizar vista
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar Sesión'
+        ]);
     }
 
     public static function logout() {
         echo "Desde Login";
     }
 
-    public static function crear() {
-        echo "Desde Crear";
+    public static function crear(Router $router) {
 
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         }
+
+        // Renderizar vista
+        $router->render('auth/crear', [
+            'titulo' => 'Crear tu cuenta en UpTask'
+        ]);
     }
 
     public static function olvide() {
