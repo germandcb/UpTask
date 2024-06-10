ca410e136b13134ad685352d3e367c9f6f37f0dc
germandc <germancelis16@gmail.com>
2024-06-10 09:11:13 -0500

    Creando Formulario de Login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,37 @@
-<h1>Login</h1>
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crear y Administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form action="/" method="POST" class="formulario">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Tu Email"
+                    name="email"
+                />
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Tu Password"
+                    name="password"
+                />
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes una cuenta? obtener una</a>
+            <a href="/olvide">¿Olvidaste tu Password?</a>
+        </div>
+    </div> <!-- .contenedor-sm -->
+</div>
