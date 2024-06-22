12a7eaf70100494e9fcc96e14a5e05c9c2a686e4
germandc <germancelis16@gmail.com>
2024-06-22 17:49:45 -0500

    verificando que los password sean iguales

--- models/Usuario.php
+++ models/Usuario.php
@@ -10,6 +10,7 @@ class usuario extends ActiveRecord {
     protected $nombre;
     protected $email;
     protected $password;
+    protected $password2;
     protected $token;
     protected $confirmado;
 
@@ -18,6 +19,7 @@ class usuario extends ActiveRecord {
         $this->nombre = $args['nombre'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? '';
         $this->token = $args['token'] ?? '';
         $this->confirmado = $args['confirmado'] ?? '';
     }
@@ -26,9 +28,17 @@ class usuario extends ActiveRecord {
         if(!$this->nombre) {
             self::$alertas['error'][] = 'El Nombre del Usuario es Obligatori';
         }
-
         if(!$this->email) {
             self::$alertas['error'][] = 'El Email del Usuario es Obligatorio';
+        }
+        if(!$this->password) {
+            self::$alertas['error'][] = 'El Password no puede ir vacio';
+        }
+        if(strlen($this->password) < 6) {
+            self::$alertas['error'][] = 'El Password debe contener almenos 6 caracteres';
+        }
+        if($this->password !== $this->password2) {
+            self::$alertas['error'][] = 'Los password son diferentes';
         }
 
         return self::$alertas;
