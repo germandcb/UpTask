26c06ff06ecf9591f009142945d4dd587b3eefe2
germandc <germancelis16@gmail.com>
2024-06-26 22:10:06 -0500

    ajustando la ubicación del metodo para enviar instrucciones

--- classes/Email.php
+++ classes/Email.php
@@ -43,6 +43,35 @@ class Email {
         $mail->send();
     }
 
+    public function enviarInstrucciones()
+    {
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'sandbox.smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = 'd3f755049e2fb1';
+        $mail->Password = 'f6f5f8dba9200d';
+
+        $mail->setFrom('cuentas@uptask.com');
+        $mail->addAddress('cuentas@uptask.com', 'uptask.com');
+        $mail->Subject = 'Reestablece tu Password';
+
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = '<html>';
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Parece que has olvidado tu password, sigue el siguiente enlace para recuperarlo</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/reestablecer?token='" . $this->token . "'> Reestablecer Password</a></p>";
+        $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>";
+        $contenido .= '</html>';
+
+        $mail->Body = $contenido;
+
+        // Enviar el email
+        $mail->send();
+    }
+
 }
 
 ?>
