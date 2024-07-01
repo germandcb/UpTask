321e61d40c836864694704888add2eab2edddd29
germandc <germancelis16@gmail.com>
2024-07-01 17:27:04 -0500

    Creando la Ruta y controller del Dashboard

--- public/index.php
+++ public/index.php
@@ -2,8 +2,9 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
-use Controllers\LoginController;
 use MVC\Router;
+use Controllers\LoginController;
+use Controllers\DashboardController;
 $router = new Router();
 
 // Login
@@ -26,6 +27,9 @@ $router->post('/reestablecer', [LoginController::class, 'reestablecer']);
 // Confirmación de cuenta 
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
 $router->get('/confirmar', [LoginController::class, 'confirmar']);
+
+// ZONA DE PROYECTOS
+$router->get('/dashboard', [DashboardController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
