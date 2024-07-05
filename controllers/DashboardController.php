d4b3906e85d439d9179d6b2e920bd078935b79d1
germandc <germancelis16@gmail.com>
2024-07-04 21:49:43 -0500

    Validando que la persona que visita el proyecto, es quien lo creo

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -54,6 +54,26 @@ class DashboardController {
         ]);
     }
 
+    public static function proyecto(Router $router) {
+        
+        session_start();
+        isAuth();
+
+        $token = $_GET['id'];
+        if (!$token) header('Location: /dashboard');
+
+        // Revisar que la persona que visita el proyecto, es quien lo creo
+        $proyecto = Proyecto::where('url', $token);
+
+        if ($proyecto->propietarioId !== $_SESSION['id']) {
+            header('Location: /dashboard');
+        }
+
+        $router->render('dashboard/proyecto', [
+            'titulo' => $proyecto->proyecto
+        ]);
+    }
+
     public static function perfil(Router $router) {
 
         session_start();
