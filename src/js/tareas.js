27a884df7c237b6244d10aa1f9758e233c6a4398
germandc <germancelis16@gmail.com>
2024-07-06 15:21:15 -0500

    Configurando botón de cerrar modal

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -33,6 +33,21 @@
             const formulario = document.querySelector('.formulario');
             formulario.classList.add('animar');
         }, 0);
+
+        modal.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            if (e.target.classList.contains('cerrar-modal')) {
+
+                const formulario = document.querySelector('.formulario');
+                formulario.classList.add('cerrar');
+                setTimeout(() => {
+                    modal.remove();
+                }, 400);
+            } 
+
+        });
+
         document.querySelector('body').appendChild(modal);
     }
 })();
