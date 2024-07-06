71bda27f1348e3796c1d17af7fcaf06c58d8985d
germandc <germancelis16@gmail.com>
2024-07-06 11:11:59 -0500

    Añadiendo js para mostrar formulario

---
+++ src/js/tareas.js
@@ -0,0 +1,9 @@
+(function() {
+    // Botón para mostrar el Modal de Agregar Tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea'); 
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        console.log('mostrando form...')
+    }
+})();
--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -11,3 +11,10 @@
     </div>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
+
+<?php 
+    $script = '
+        <script src="build/js/tareas.js"> </script>
+        <script src="build/js/app.js"> </script>
+    ';
+?>
