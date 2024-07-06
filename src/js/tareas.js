1769f8685bb236e9b74f7c2d6652f3f4792d9b5b
germandc <germancelis16@gmail.com>
2024-07-06 11:25:06 -0500

    Creando el Modelo para añadir Tareas

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -4,6 +4,30 @@
     nuevaTareaBtn.addEventListener('click', mostrarFormulario);
 
     function mostrarFormulario() {
-        console.log('mostrando form...')
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea"> 
+                <legend>Añadir una nueva tarea</legend>
+                <div class="campo"> 
+                    <label>Tarea</label>
+                    <input
+                        type="text"
+                        name="tarea"
+                        placeholder="Añadir Tarea al Proyecto Actual"
+                        id="tarea"
+                    />
+                </div>
+                <div class="opciones">
+                    <input 
+                        type="submit"
+                        calss="submit-nueva-tarea"
+                        value="Añadir Tarea"
+                    />
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form> 
+        `;
+        document.querySelector('body').appendChild(modal);
     }
 })();
