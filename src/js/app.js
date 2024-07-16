3f2312c94a397f313bc89eb1c52c96648e3c0653
germandc <germancelis16@gmail.com>
2024-07-15 22:40:26 -0500

    Mostrando y ocultando el menú en movil

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,20 @@
 const mobileMenuBtn = document.querySelector('#mobile-menu');
 const sidebar = document.querySelector('.sidebar');
+const cerrarMenu = document.querySelector('#cerrar-menu');
 
 if(mobileMenuBtn) {
     mobileMenuBtn.addEventListener('click', function() {
         sidebar.classList.toggle('mostrar');
+    });
+}
+
+if(cerrarMenu) {
+    cerrarMenu.addEventListener('click', function() {
+        sidebar.classList.add('ocultar');
+
+        setTimeout(() => {
+            sidebar.classList.remove('mostrar');
+            sidebar.classList.remove('ocultar');
+        }, 500)
     });
 }
